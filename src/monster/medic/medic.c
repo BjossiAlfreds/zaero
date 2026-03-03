641132cc08a31bcd1beb855d2cc8d939cd0957c1
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-03-03 08:46:32 +0000

    game: Fixed monster AI breaking in certain cases after being healed by a med ic
    If a medic had no player enemy when healing a monster, FoundTarget() would not be called if the monster had an enemy at the time of death, causing him to be in a buggy stand state

--- src/monster/medic/medic.c
+++ src/monster/medic/medic.c
@@ -843,6 +843,10 @@ medic_cable_attack(edict_t *self)
 			self->enemy->enemy = self->oldenemy;
 			FoundTarget(self->enemy);
 		}
+		else if (self->enemy->enemy)
+		{
+			FoundTarget(self->enemy);
+		}
 	}
 	else
 	{
