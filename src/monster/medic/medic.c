a079c5ee92e48a0036d12adcd4db25a295bc4c5c
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-03-03 14:15:19 +0000

    game: Fixed medics not healing monsters with flies buzzing
    findDeadMonster rejected all monsters that are thinking, so adding exceptions for M_FliesOn and M_FliesOff think functions avoids this problem

--- src/monster/medic/medic.c
+++ src/monster/medic/medic.c
@@ -8,6 +8,9 @@
 #include "../../header/local.h"
 #include "medic.h"
 
+void M_FliesOn(edict_t *self);
+void M_FliesOff(edict_t *self);
+
 static int sound_idle1;
 static int sound_pain1;
 static int sound_pain2;
@@ -59,7 +62,7 @@ medic_FindDeadMonster(edict_t *self)
 			continue;
 		}
 
-		if (ent->nextthink)
+		if (ent->nextthink && (ent->think != M_FliesOn && ent->think != M_FliesOff))
 		{
 			continue;
 		}
@@ -71,6 +74,7 @@ medic_FindDeadMonster(edict_t *self)
 
 		if (!best)
 		{
+
 			best = ent;
 			continue;
 		}
@@ -820,6 +824,9 @@ medic_cable_attack(edict_t *self)
 	}
 	else if (self->s.frame == FRAME_attack50)
 	{
+		self->enemy->s.effects &= ~EF_FLIES;
+		self->enemy->s.sound = 0;
+
 		self->enemy->spawnflags = 0;
 		self->enemy->monsterinfo.aiflags = 0;
 		self->enemy->target = NULL;
