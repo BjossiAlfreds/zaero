f6eb145f053d1be499e5ae40c136d5d266e8ae2e
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-03-04 14:56:43 +0000

    game: Fixed dead bodies glowing too late when healed by medics
    AI_RESURRECTING is set when the hook hits the body but had no effect since M_SetEffects was not called

--- src/monster/medic/medic.c
+++ src/monster/medic/medic.c
@@ -10,6 +10,7 @@
 
 void M_FliesOn(edict_t *self);
 void M_FliesOff(edict_t *self);
+void M_SetEffects(edict_t *self);
 
 static int sound_idle1;
 static int sound_pain1;
@@ -821,6 +822,7 @@ medic_cable_attack(edict_t *self)
 	{
 		gi.sound(self->enemy, CHAN_AUTO, sound_hook_hit, 1, ATTN_NORM, 0);
 		self->enemy->monsterinfo.aiflags |= AI_RESURRECTING;
+		M_SetEffects(self->enemy);
 	}
 	else if (self->s.frame == FRAME_attack50)
 	{
@@ -844,6 +846,7 @@ medic_cable_attack(edict_t *self)
 		}
 
 		self->enemy->monsterinfo.aiflags |= AI_RESURRECTING;
+		M_SetEffects(self->enemy);
 
 		if (self->oldenemy && self->oldenemy->client)
 		{
@@ -888,6 +891,7 @@ medic_hook_retract(edict_t *self)
 
 	gi.sound(self, CHAN_WEAPON, sound_hook_retract, 1, ATTN_NORM, 0);
 	self->enemy->monsterinfo.aiflags &= ~AI_RESURRECTING;
+	M_SetEffects(self->enemy);
 }
 
 mframe_t medic_frames_attackCable[] = {
