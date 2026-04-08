0e4ec6e926239341ab344fd7cfb6c8395cb2f482
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-04-08 15:52:04 +0000

    game: Fixed monsters running in place if they are in combat point mode and enemy is very close
    M_MoveToGoal doesn't act if the enemy is within step distance. This check is only appropriate if the goalentity is the same as the enemy

--- src/monster/misc/move.c
+++ src/monster/misc/move.c
@@ -647,7 +647,8 @@ M_MoveToGoal(edict_t *ent, float dist)
 	}
 
 	/* if the next step hits the enemy, return immediately */
-	if (ent->enemy && SV_CloseEnough(ent, ent->enemy, dist))
+	if (ent->enemy && ent->enemy == ent->goalentity &&
+		SV_CloseEnough(ent, ent->enemy, dist))
 	{
 		return;
 	}
